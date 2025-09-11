82620b789d133809278a53e015c43f5bcd85fdad
Voitenko Illia <79865476+IlyaVoitenko@users.noreply.github.com>
2025-09-11 13:40:26 +0200

    removed useless categories

--- controllers/translations.js
+++ controllers/translations.js
@@ -1,19 +1,7 @@
 const { ctrlWrappen } = require("../decorators");
-// const i18next = require("../i18next");
 const path = require("path");
 const fs = require("fs");
 
-// const getDataTranslation = async (req, res) => {
-//   console.log("ff");
-//   const lang = req.params.lang || "de";
-//   await i18next.changeLanguage(lang, (err, t) => {
-//     if (err)
-//       return res.status(500).json({ error: "Failed to change language" });
-
-//     const translations = t("header.info.HowToOrder");
-//     res.json(translations);
-//   });
-// };
 const getTranslation = (req, res) => {
   const { locale } = req.params;
   const filePath = path.join(
@@ -29,6 +17,5 @@ const getTranslation = (req, res) => {
   else res.status(404).send("File not found");
 };
 module.exports = {
-  // getDataTranslation: ctrlWrappen(getDataTranslation),
   getTranslation: ctrlWrappen(getTranslation),
 };
